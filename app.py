37aa8c0e4076502c381ab49db102884718972f65
Nilai Vemula <nilaivemula@gmail.com>
2021-02-07 15:39:45 -0600

    just new app version

--- app.py
+++ app.py
@@ -1,112 +1,35 @@
-from flask import Flask, url_for, session
+import functools
+import json
+import os
+
+import flask
 from flask import render_template, redirect
-from authlib.integrations.flask_client import OAuth
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-import base64
-from bs4 import BeautifulSoup
+from authlib.integrations.requests_client import OAuth2Session
+import google.oauth2.credentials
+import googleapiclient.discovery
 
-app = Flask(__name__)
+import google_auth
+import gmail_auth
+
+app = flask.Flask(__name__)
 app.secret_key = '!secret'
-app.config.from_object('config')
 
-CONF_URL = 'https://accounts.google.com/.well-known/openid-configuration'
-oauth = OAuth(app)
-oauth.register(
-    name='google',
-    server_metadata_url=CONF_URL,
-    client_kwargs={
-        'scope': 'openid email profile https://www.googleapis.com/auth/gmail.readonly'
-    }
-)
-
-def get_emails(token):
-    # Connect to the Gmail API
-    service = build('gmail', 'v1', credentials=token['access_token'])
-
-    # request a list of all the messages
-    result = service.users().messages().list(userId='me').execute()
-
-    # We can also pass maxResults to get any number of emails. Like this:
-    # result = service.users().messages().list(maxResults=200, userId='me').execute()
-    messages = result.get('messages')
-
-    # messages is a list of dictionaries where each dictionary contains a message id.
-
-    # iterate through all the messages
-    for msg in messages:
-        # Get the message from its id
-        txt = service.users().messages().get(
-            userId='me', id=msg['id']).execute()
-
-        # Use try-except to avoid any Errors
-        try:
-            # Get value of 'payload' from dictionary 'txt'
-            payload = txt['payload']
-            headers = payload['headers']
-
-            # Look for Subject and Sender Email in the headers
-            for d in headers:
-                if d['name'] == 'Subject':
-                    subject = d['value']
-                if d['name'] == 'From':
-                    sender = d['value']
-
-            # The Body of the message is in Encrypted format. So, we have to decode it.
-            # Get the data and decode it with base 64 decoder.
-            parts = payload.get('parts')[0]
-            data = parts['body']['data']
-            data = data.replace("-", "+").replace("_", "/")
-            decoded_data = base64.b64decode(data)
-
-            # Now, the data obtained is in lxml. So, we will parse
-            # it with BeautifulSoup library
-            soup = BeautifulSoup(decoded_data, "lxml")
-            body = soup.body()
-
-            # Printing the subject, sender's email and message
-            print("Subject: ", subject)
-            print("From: ", sender)
-            print("Message: ", body)
-            print('\n')
-        except:
-            pass
-
+app.register_blueprint(google_auth.app)
+app.register_blueprint(gmail_auth.app)
 
 
 @app.route('/')
-def homepage():
-    user = session.get('user')
-    token = session.get('token')
-    if user:
-        print(user)
-        print(token)
-        emails = get_emails(token)
-        
+def index():
+    if google_auth.is_logged_in():
+        return flask.render_template('list.html', user_info=google_auth.get_user_info())
 
-    return render_template('home.html', user=user)
+    return render_template('root.html')
 
 
-@app.route('/login')
-def login():
-    redirect_uri = url_for('auth', _external=True)
-    return oauth.google.authorize_redirect(redirect_uri)
-
-
-@app.route('/auth')
-def auth():
-    token = oauth.google.authorize_access_token()
-    user = oauth.google.parse_id_token(token)
-    session['user'] = user
-    session['token'] = token
-    return redirect('/')
-
-
-@app.route('/logout')
-def logout():
-    session.pop('user', None)
-    return redirect('/')
+@app.route('/emails')
+def emails():
+    labels = gmail_auth.get_emails()
+    return labels
 
 
 if __name__ == '__main__':
