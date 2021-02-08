5b5d74ee19e825022fdf4141ad6f9863767f6b72
Nilai Vemula <nilaivemula@gmail.com>
2021-02-07 18:56:24 -0600

    used iframes to render emails

--- gmail.py
+++ gmail.py
@@ -9,7 +9,6 @@ from google_auth import build_credentials, get_user_info
 
 import base64
 
-from find_html import find_mime_type
 
 app = Blueprint('gmail', __name__)
 
@@ -35,38 +34,20 @@ def get_emails():
     data_to_display = []
 
     for id in ids:
-        
+
         try:
-            
-            
+
             email = {}
 
             request = service.users().messages().get(userId='me', id=id)
             response = request.execute()
 
-            # mime_type = "text/html"
-            # try:
-            #     key_chain = find_mime_type(response, mime_type)
-            # except KeyError:
-            #     print('Could not find this mime type: {0}'.format(mime_type))
-            #     exit()
-            # print('Found {0} mime type here: {1}'.format(mime_type, key_chain))
-            # nested = response
-            # for key in key_chain:
-            #     nested = nested[key]
-            # print('Confirmation lookup: {0}'.format(nested))
-
-            
-            
-            
-            
             headers = list(response["payload"]["headers"])
-            
+
             looking_for = ["Date", "Subject", "From", "To"]
             for header in headers:
                 if header["name"] in looking_for:
                     email[header["name"]] = header["value"]
-                
 
             #email["Subject"] = response["payload"]["headers"][3]["value"]
             #email["From"] = response["payload"]["headers"][4]["value"]
@@ -75,12 +56,18 @@ def get_emails():
             try:
 
                 base64_message = response["payload"]["parts"][0]["parts"][0]["body"]["data"]
-                
+
             except (KeyError, TypeError) as e:
                 try:
-                    base64_message = response["payload"]["parts"][0]["body"]["data"]
-                except (KeyError, TypeError) as e:
-                    base64_message = "Ti9B"
+                    base64_message = response["payload"]["parts"][1]["body"]["data"]
+                except (KeyError, TypeError, IndexError) as e:
+                    try:
+                        base64_message = response["payload"]["parts"][0]["body"]["data"]
+                    except (KeyError, TypeError, IndexError) as e:
+                        try:
+                            base64_message = response["payload"]["body"]["data"]
+                        except (KeyError, TypeError, IndexError) as e:
+                            base64_message = "Ti9B"
             email["body"] = base64.urlsafe_b64decode(
                 base64_message).decode('utf-8')
         except HTTPError as e:
